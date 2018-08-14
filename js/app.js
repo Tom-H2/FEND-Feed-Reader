fab3851c6d3fb5b3d798474a6fa2a1f32c78ce56
Thomas Harris <lostmt.tom@gmail.com>
2018-08-14 08:02:33 -0700

    tests: Un-comments allFeeds object after checking Jasmine suite

--- js/app.js
+++ js/app.js
@@ -9,11 +9,10 @@
 // The names and URLs to all of the feeds we'd like available.
 var allFeeds = [
     {
-        //name: 'Udacity Blog',
+        name: 'Udacity Blog',
         url: 'http://blog.udacity.com/feed'
     }, {
-        //name: 'CSS Tricks',
-        name: '',
+        name: 'CSS Tricks',
         url: 'http://feeds.feedburner.com/CssTricks'
     }, {
         name: 'HTML5 Rocks',
