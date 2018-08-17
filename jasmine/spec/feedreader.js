ce604f51e8ab5e4cf06e28b49b593fb3e7fa2bb5
Thomas Harris <lostmt.tom@gmail.com>
2018-08-17 07:49:41 -0700

    tests: Fixes the 'Initial Entries' test to more accurately match the specification per suggestion from reviewer

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,6 +1,5 @@
 
 $(function () {
-  let feed = document.querySelector('.feed');
 	describe('RSS Feeds', function () { //This test suite was given as starter code
 		it('are defined', function () { //part of starter code and mimiced for test comprosing the rest of the suite
 			expect(allFeeds).toBeDefined();
@@ -33,24 +32,26 @@ $(function () {
 		});
 	});
 	describe('Initial Entries', function () { //creates test suite
-		beforeEach(function (done) { //writes code that will run loadFeed before the expect test
+    const feed = document.querySelector('.feed');
+    beforeEach(function (done) { //writes code that will run loadFeed before the expect test
 			loadFeed(0, done); //followed Jasmine documentation for asynchronous work
 		});
 		it('load complete with at least single entry', function () { //test that expects that when the entries load the number loaded will be greater than 0
-			expect(feed.length < 0).toBe(false); //Tests that there is at least a single entry
+			expect(feed.children.length < 0).toBe(false); //Tests that there is at least a single entry
 		});
 	});
 	describe('New Feed Selection', function () { //Required test suite
-		initialFeed = []; //creates empty array into which to push the initial feed: see line 54
+    const feed = document.querySelector('.feed');
+    initialFeed = []; //creates empty array into which to push the initial feed: see line 54
 		nextFeed = []; //creates empty array into which to push the next feed: see line 59
 		beforeEach(function (done) { //writes code that will run loadFeed before the expect test
 			loadFeed(0, function() { //followed Jasmine documentation for asynchronous work
 			initialFeed.push(feed.children[0].innerText); //This pushes the first feed into an empty array
-      });
         loadFeed(1, function() { //followed callback article from codeburst.io per instructions from first reviewer
 			  nextFeed.push(feed.children[0].innerText); // This pushes the second feed into another array
         done();
         });
+      });
 		});
 		it('content actually changes', function () {
 			expect(initialFeed).not.toEqual(nextFeed); //This expectation compares the feed in the first array to the feed in the second. If they are not the same the test is green
